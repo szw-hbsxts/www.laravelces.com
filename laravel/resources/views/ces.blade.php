f089840a3e3f165437d9d3eb96992a3cdd18715f
szwhbsxts <szw1314y@163.com>
2021-01-26 18:02:00 +0800

    all

---
+++ laravel/resources/views/ces.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>我是测试页？</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/ces">测试01</a></li>
+        <li><a href="/welcome">欢迎来到</a></li>
+        <li><a href="#">测试01</a></li>
+        <li><a href="#">测试01</a></li>
+        <li><a href="#">测试01</a></li>
+        <li><a href="#">测试01</a></li>
+        <li><a href="#">测试01</a></li>
+    </ul>
+</body>
+</html>
